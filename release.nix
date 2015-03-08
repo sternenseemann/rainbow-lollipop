b90a242172c0570d1133f9aa50907b88b94ec4d6
grindhold <grindhold@gmx.net>
2015-03-08 22:54:00 +0100

    fix nix file

--- release.nix
+++ release.nix
@@ -6,10 +6,9 @@ let
             releaseTools.sourceTarball {
                 name = "rl-tarball";
                 version = "0.0.1";
-                src = rainbow-lollipop;
+                src = ./.;
                 
-                buildInputs = [cmake vala_0_26 pkgconfig]
-                
+                buildInputs = [cmake vala_0_26 pkgconfig];
             };
     };
 in jobs
