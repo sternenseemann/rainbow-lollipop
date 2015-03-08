f634060bbaf6b3e363adfb61c8c1f2baf63dd136
grindhold <grindhold@gmx.net>
2015-03-08 23:01:01 +0100

    add build deps to nixfile

--- release.nix
+++ release.nix
@@ -8,7 +8,9 @@ let
                 version = "0.0.1";
                 src = ./.;
                 
-                buildInputs = [cmake vala_0_26 pkgconfig];
+                buildInputs = [cmake vala_0_26 zeromq2 pkgconfig glib gtk3 clutter_gtk webkitgtk
+                               gnome3.libgee sqlite udev xorg.libpthreadstubs xorg.libXdmcp
+                               xorg.libxshmfence libxkbcommon];
             };
     };
 in jobs
