b45e2e96a92d7198646bb8f4ddd917dbc233d7d6
grindhold <grindhold@gmx.net>
2015-03-09 01:56:48 +0100

    added windows target to nixfile

--- release.nix
+++ release.nix
@@ -1,10 +1,21 @@
 let 
-    pkgs = import <nixpkgs> {};
+    pkgs_linux = import <nixpkgs> {};
+    pkgs_windows = import <nixpkgs> {
+        crossSystem = {
+            config = "x86_64-w64-mingw32";
+            arch = "x86_64";
+            libc = "msvcrt";
+            platform = {};
+            openssl.system = "mingw64";
+        };
+
+    };
+    system = "x86_64-linux";
     jobs = rec {
         tarball = 
-            with pkgs;
-            releaseTools.sourceTarball {
-                name = "rl-tarball";
+            with pkgs_linux;
+            stdenv.mkDerivation {
+                name = "rainbow-lollipop-tarball";
                 version = "0.0.1";
                 src = ./.;
                 
@@ -12,5 +23,16 @@ let
                                gnome3.libgee sqlite udev xorg.libpthreadstubs xorg.libXdmcp
                                xorg.libxshmfence libxkbcommon];
             };
+        windows_64 =
+            with pkgs_windows;
+            (stdenv.mkDerivation {
+                name = "rainbow-lollipop-windows";
+                version = "0.0.1";
+                src = ./.;
+                
+                buildInputs = [cmake vala_0_26 zeromq2 pkgconfig glib gtk3 clutter_gtk webkitgtk
+                               gnome3.libgee sqlite udev xorg.libpthreadstubs xorg.libXdmcp
+                               xorg.libxshmfence libxkbcommon];
+            }).crossDrv;
     };
 in jobs
